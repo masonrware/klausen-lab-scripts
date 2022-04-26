adc3bbd62135d4194b353ca29a5bbe7c846398ab
masonrware <86129546+masonrware@users.noreply.github.com>
2022-04-26 17:37:30 -0400

    try

--- associates_parser.py
+++ associates_parser.py
@@ -34,7 +34,7 @@ class File:
         self.regex_links: dict() = {
             'regex_1d_links': {
                 'COORDINATOR OF': ['coordinat'],
-                'FINANCIAL/LOGISTIC SUPPORTER OF': ['financial', 'logistic'],
+                'FINANCIAL/LOGISTICAL SUPPORTER OF': ['financial', 'logistic'],
                 'LEADER OF': ['leader'],
                 'RECRUITER OF': ['recruiter'],
                 'SOCIAL MEDIA FOLLOWER': ['social media follower'],
